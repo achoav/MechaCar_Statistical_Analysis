fdb064575e4859e58b6e785dc73b2038933b210f
Valeria Achoa <73545138+achoav@users.noreply.github.com>
2021-03-15 00:27:41 -0400

    update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,7 @@ mechaCarData <- read_csv("Resources/MechaCar_mpg.csv") #import MechaCar data
 head(mechaCarData) #display first rows of imported data
 
 # Perform linear regression using the lm() function
+# on 6 variables: vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=mechaCarData) 
+#generate summary statistics
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=mechaCarData)) 
