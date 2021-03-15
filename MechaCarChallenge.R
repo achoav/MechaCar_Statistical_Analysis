db73d6230d771619deb62911c080ea3b056d8c16
Valeria Achoa <73545138+achoav@users.noreply.github.com>
2021-03-15 00:18:04 -0400

    update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,9 @@
-library(tidyverse) #import tidyverse package
+# Please install.packages("tidyverse") before running it
+# Or just install install.packages("dplyr")
+
+library("dplyr") #import dplyr package
 
 mechaCarData <- read_csv("Resources/MechaCar_mpg.csv") #import MechaCar data
 head(mechaCarData) #display first rows of imported data
 
+# Perform linear regression using the lm() function
