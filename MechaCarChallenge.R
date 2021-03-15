066c2bd13d3f642f40fd9a6a600dd17e91574dd3
Valeria Achoa <73545138+achoav@users.noreply.github.com>
2021-03-15 00:10:59 -0400

    update

---
+++ MechaCarChallenge.R
@@ -0,0 +1,5 @@
+library(tidyverse) #import tidyverse package
+
+mechaCarData <- read_csv("Resources/MechaCar_mpg.csv") #import MechaCar data
+head(mechaCarData) #display first rows of imported data
+
